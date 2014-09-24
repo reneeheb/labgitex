abc089e982c631e0f8bc9331f8e63a917769c7cf
Dan-Ghenadie ROMAN <danghenadieroman@gmail.com>
2014-09-24 12:09:14 -0300

    verification de commit

--- src/lab3/Lab3.java
+++ src/lab3/Lab3.java
@@ -17,6 +17,7 @@ public class Lab3 {
      */
     public static void main(String[] args) {
         System.out.println("Bonjour Renée!");
+        System.out.println("Bonjour Dan!");
         
     }
     
