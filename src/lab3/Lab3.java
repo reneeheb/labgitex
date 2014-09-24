0a256f46725d6a5518f2535d6b56e29c4b987a45
Renee Hebert <hebertrenee25@hotamil.com>
2014-09-24 11:10:24 -0300

    Ajouter une ligne d'impression dans le main

--- src/lab3/Lab3.java
+++ src/lab3/Lab3.java
@@ -16,7 +16,8 @@ public class Lab3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Bonjour Renée!");
+        
     }
     
 }
