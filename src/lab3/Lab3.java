39a9e1482d6fdee1cd74d9d69bcdc9d38dc70de2
Dan-Ghenadie Roman <danghenadieroman@gmail.com>
2014-10-01 11:08:53 -0400

    modif 1 oct

--- src/lab3/Lab3.java
+++ src/lab3/Lab3.java
@@ -7,10 +7,11 @@ public class Lab3 {
     public static void main(String[] args) {
         System.out.println("Bonjour Renée!");
         System.out.println("Bonjour Dan!");
-        
+
         System.out.println("Modification 25/sep (1)");
-        
-        
-    }
-    
+        System.out.println("Modification 01 oct");
+        System.out.println("Modification 01 oct");
+            }
+
 }
+
